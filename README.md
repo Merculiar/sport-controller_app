89c7ba205fc7455ec8b2f4724f5afdbc13a062be
Vlad Sumin <vsumin@exoft.net>
2024-03-25 00:32:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sport_controller_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/sport_controller_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sport_controller_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
